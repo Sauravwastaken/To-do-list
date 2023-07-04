2664ca076d2acf76d63bf72fd1407729b1006d2a
Saurav <sauravthecoder@gmail.com>
2023-07-04 19:00:25 +0530

    UPdate all

---
+++ assets/index.js
@@ -0,0 +1,58 @@
+let userInput = document.getElementsByTagName("input")[0];
+let btn = document.getElementsByTagName("button")[0];
+let taskContainer = document.getElementsByTagName("ul")[0];
+let deleteBtn = `<img class="delete" src="del.png"/>
+`;
+
+showData();
+
+btn.addEventListener("click", (e) => {
+  if (userInput.value != "") {
+    userInput.placeholder = "What's the task today?";
+    // Create list
+    let li = document.createElement("li");
+    li.classList.add("task");
+
+    // Create delete btn
+    let img = document.createElement("img");
+    img.src = "assets/del.png";
+    img.classList.add("delete");
+
+    // Create Checkbox
+    // let checkIcon = document.createElement("input");
+    // checkIcon.type = "checkbox";
+    // checkIcon.classList.add("checkbox");
+
+    // Insert into DOM
+    li.innerHTML = userInput.value + deleteBtn;
+    userInput.value = "";
+
+    taskContainer.appendChild(li);
+    // li.appendChild(checkIcon);
+  } else {
+    userInput.placeholder = "Type Something first...";
+  }
+
+  saveData();
+});
+
+taskContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+  } else if (e.target.tagName === "IMG") {
+    e.target.parentElement.style.scale = "0";
+    setTimeout(() => {
+      e.target.parentElement.remove();
+      saveData();
+    }, 300);
+  }
+  saveData();
+});
+
+function showData() {
+  let data = localStorage.getItem("data");
+  taskContainer.innerHTML = data;
+}
+
+function saveData() {
+  localStorage.setItem("data", taskContainer.innerHTML);
+}
